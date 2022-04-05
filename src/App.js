c478d9efaad97e9d06fb801de8e14d575fdcd7ab
Kalana Dissanayake <kalanadis@gmail.com>
2022-04-05 17:41:43 +0530

    feature with error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Text Changed on feature branch!!
+          Text Changed on feature branch XX!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          XXX
         </a>
       </header>
     </div>
